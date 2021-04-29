7a9fa76859e5df88cc514bf76d3170d852596151
spatiality-dc <dan@dinosaurmedia.org>
2021-04-30 07:11:50 +1000

    updated select buttons on to add or remove class name when active

--- js/session_storage.js
+++ js/session_storage.js
@@ -30,37 +30,84 @@ const explore = document.getElementById("Explore"); // Accessing The Button //
 explore.addEventListener("click", exploreTheme); // Adding event to call function when clicked // 
 
 
+
+// //check storage if funder mode was on or off
+// function checkTheme() {
+
+// if (sessionStorage.getItem("mode") == "funder") {
+//     funderTheme(); //if funder mode was on, run this function
+//   } else if (sessionStorage.getItem("mode") == "academic") {
+//     academicTheme(); //if funder mode was on, run this function
+//   } else if (sessionStorage.getItem("mode") == "practitioner") {
+//     practitionerTheme(); //if funder mode was on, run this function
+//   } else if (sessionStorage.getItem("mode") == "student") {
+//     studentTheme(); //if funder mode was on, run this function
+//   } else {
+//     exploreTheme(); //else run this function
+//   }
+
+// }
+
+
 // Checking to see if the buttons work
+// function funderTheme() { 
+//     console.log("funderTheme active");
+//     document.getElementById("modeExplainer").innerHTML = '<p>Funder mode on!<p>';
+//     document.getElementById("Funder").className = "mode-active";
+//     sessionStorage.setItem("mode", "funder");
+// };
+
+
 function funderTheme() { 
     console.log("funderTheme active");
-    document.getElementById("modeExplainer").innerHTML = '<p>Funder mode on!<p>';
+    document.getElementById("Funder").className = "mode-active";
     sessionStorage.setItem("mode", "funder");
+    document.getElementById("Academic").className = "regular-button-blue";
+    document.getElementById("Practitioner").className = "regular-button-blue";
+    document.getElementById("Student").className = "regular-button-blue";
+    document.getElementById("Explore").className = "regular-button-blue";
 };
 
 // Checking to see if the buttons work
 function academicTheme() { 
     console.log("academicTheme active");
-    document.getElementById("modeExplainer").innerHTML = '<p>Academic mode on!<p>';
+    document.getElementById("Academic").className = "mode-active";
     sessionStorage.setItem("mode", "academic");
+    document.getElementById("Funder").className = "regular-button-blue";
+    document.getElementById("Practitioner").className = "regular-button-blue";
+    document.getElementById("Student").className = "regular-button-blue";
+    document.getElementById("Explore").className = "regular-button-blue";
 };
 
 // Checking to see if the buttons work
 function practitionerTheme() { 
     console.log("practitionerTheme active"); 
-    document.getElementById("modeExplainer").innerHTML = '<p>Practitioner mode on!<p>';
+    document.getElementById("Practitioner").className = "mode-active";
     sessionStorage.setItem("mode", "practitioner");
+    document.getElementById("Funder").className = "regular-button-blue";
+    document.getElementById("Academic").className = "regular-button-blue";
+    document.getElementById("Student").className = "regular-button-blue";
+    document.getElementById("Explore").className = "regular-button-blue";
 };
 
 // Checking to see if the buttons work
 function studentTheme() { 
     console.log("studentTheme active");  
-    document.getElementById("modeExplainer").innerHTML = '<p>Student mode on!<p>';
+    document.getElementById("Student").className = "mode-active";
     sessionStorage.setItem("mode", "student");
+    document.getElementById("Funder").className = "regular-button-blue";
+    document.getElementById("Academic").className = "regular-button-blue";
+    document.getElementById("Practitioner").className = "regular-button-blue";
+    document.getElementById("Explore").className = "regular-button-blue";
 };
 
 // Checking to see if the buttons work
 function exploreTheme() { 
     console.log("exploreTheme active");  
-    document.getElementById("modeExplainer").innerHTML = '<p>Explore mode on!<p>';
+    document.getElementById("Explore").className = "mode-active";
     sessionStorage.setItem("mode", "explore");
+    document.getElementById("Funder").className = "regular-button-blue";
+    document.getElementById("Academic").className = "regular-button-blue";
+    document.getElementById("Practitioner").className = "regular-button-blue";
+    document.getElementById("Student").className = "regular-button-blue";
 };
